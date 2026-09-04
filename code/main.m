3c241843a852a20c04ce4e8ba5b10b1523d3b0ab
mkurvink <maximkurvink@gmail.com>
2026-09-04 11:54:43 +0200

    Running preprocessing meta table
    The meta file created is not committed as its contents is too large for subsequent pushes to the main branch

--- code/main.m
+++ code/main.m
@@ -1,23 +1,28 @@
-addpath('utils')
 clear all; close all; clc;
+
+codeDir = fileparts(mfilename('fullpath'));
+cd(codeDir)
+addpath(genpath(fullfile(codeDir, 'utils')))
 
 % put all the studies together and preprocess the data
 preprocess_create_master_table()
 
-% weighted, flexible-degree polynomial fitting to estimate scatter (+model-free bin estimates)
-estimate_polynomial_mvav()
+% % weighted, flexible-degree polynomial fitting to estimate scatter (+model-free bin estimates)
+% estimate_polynomial_mvav()
 
-% summarize the datasets
-table1()
+% % summarize the datasets
+% table1()
 
-% create figures
-figure2()
-figure3A()
-figure3B_S7()
-figure4()
-figureS1()
-figureS2to5()
-figureS6()
+% % create figures
+% figure2()
+% figure3A()
+% figure3B_S7()
+% figure4()
+% figureS1()
+% figureS2to5()
+% figureS6()
 
-% stats using LMMs
-linear_mixed_models()
+% % stats using LMMs
+% linear_mixed_models()
+
+cd(fullfile(codeDir, '..', 'results'))

d570fb10c2de6bce17db9db111dd34bd689c7516
mkurvink <maximkurvink@gmail.com>
2026-09-10 18:02:35 +0200

    Running estimate_polynomial_mvav and table1
    This commit contains the adapted main and the results, including all figures after running estimate_polynomial_mvav and table1.

--- code/main.m
+++ code/main.m
@@ -4,23 +4,23 @@ codeDir = fileparts(mfilename('fullpath'));
 cd(codeDir)
 addpath(genpath(fullfile(codeDir, 'utils')))
 
-% put all the studies together and preprocess the data
-preprocess_create_master_table()
+% % put all the studies together and preprocess the data
+% preprocess_create_master_table()
 
-% % weighted, flexible-degree polynomial fitting to estimate scatter (+model-free bin estimates)
-% estimate_polynomial_mvav()
+% weighted, flexible-degree polynomial fitting to estimate scatter (+model-free bin estimates)
+estimate_polynomial_mvav()
 
-% % summarize the datasets
-% table1()
+% summarize the datasets
+table1()
 
-% % create figures
-% figure2()
-% figure3A()
-% figure3B_S7()
-% figure4()
-% figureS1()
-% figureS2to5()
-% figureS6()
+% create figures
+figure2()
+figure3A()
+figure3B_S7()
+figure4()
+figureS1()
+figureS2to5()
+figureS6()
 
 % % stats using LMMs
 % linear_mixed_models()
